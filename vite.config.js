ff2aef1201d8052fe4094a21c39208380ad8e949
lutopia-official <iop327866@gmail.com>
2025-12-29 01:40:33 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base 設定為您的 GitHub 儲存庫名稱，前後需加上斜線
-  // 這能確保部署到 GitHub Pages 後，網頁路徑不會出錯
-  base: '/toen-pos/',
-  
-  // 如果您在開發過程中需要特定的伺服器設定，可以加在 server 區塊
-  server: {
-    port: 5173,
-    host: true, // 允許透過區域網路（手機/平板）連線預覽
-  }
 })
